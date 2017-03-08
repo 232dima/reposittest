82cdd5e07a858a5cbcb3920614b8b87929ca4270
232dima <232dima@gmail.com>
2017-03-08 02:21:54 +0200

    1

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,8 @@ public class Hello {
     public static void main (String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        int a = 5;
+        System.out.println(a);
+
     }
 }
